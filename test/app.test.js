c9a8f7371db477685ca26ceb4d5e6dd5a938d9d7
shai-sivakumar <s225521115@deakin.edu.au>
2026-09-14 14:23:31 +0800

    Add basic test for Goof App

---
+++ test/app.test.js
@@ -0,0 +1,7 @@
+const assert = require('assert');
+
+describe('Goof App Basic Test', function () {
+  it('should return true for basic assertion', function () {
+    assert.strictEqual(true, true);
+  });
+});
